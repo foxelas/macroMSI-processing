86a6d969c6fe5d2c0cd720255536c6574dd81561
foxelas <foxeln@outlook.com>
2019-03-07 01:20:41 +0900

    add lda, qda

--- src/python/main.py
+++ src/python/main.py
@@ -1,5 +1,7 @@
 from os.path import dirname, join as pjoin
 from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
 import numpy as np
 import scipy.io as sio 
 import pandas as pd
@@ -78,31 +80,63 @@ is_benign = np.array([id_struct[x].IsBenign for x in range(msiN) ])
 is_cut = np.array([id_struct[x].IsCut for x in range(msiN) ])
 is_fixed = np.array([id_struct[x].IsFixed for x in range(msiN) ])
 
+###################Measured Data######
+
+measured_spectra, unique_ids = np.unique(spectra, return_index=True, axis=0)
+measuredN = len(measured_spectra)
+labels = is_benign[unique_ids]
+label_dict = {0: 'Malignant', 1: 'Benign'}
+benign_ids = get_indexes(1, is_benign[unique_ids])
+malignant_ids = get_indexes(0, is_benign[unique_ids])
+
+def plot_da(X, title):
+	if 'Principal' in title:
+		tag = 'PC'
+	elif 'Linear' in title:
+		tag = 'LD'
+	elif 'Quadratic' in title:
+		tag = 'QD'
+	else:
+		tag = ''
+
+	plt.figure()
+	for label, marker, color in zip(range(2), ('s', 'o'), ('red', 'green')):
+		plt.scatter(x=X[:,0][labels == label], 
+					y=X[:,1][labels == label] * -1, #To flip the plot
+					marker=marker,
+					color=color,
+					alpha=0.7,
+					label=label_dict[label])
+	plt.xlabel(tag + '1')
+	plt.ylabel(tag + '2')
+	leg = plt.legend(loc='upper right', fancybox=True)
+	leg.get_frame().set_alpha(0.7)
+	plt.title(title)
+
+	plt.grid()
+	plt.tight_layout
+	plt.show()
 
 ##################PCA#################
 pca = PCA(n_components=2)
 
 #Measured Spectra PCA 
-measured_spectra, unique_ids = np.unique(spectra, return_index=True, axis=0)
-benign_ids = get_indexes(1, is_benign[unique_ids])
-malignant_ids = get_indexes(0, is_benign[unique_ids])
 pca.fit(measured_spectra)
 measured_pca = pca.transform(measured_spectra)
 print("original dimension: ", measured_spectra.shape)
 print("pca-projected dimension ", measured_pca.shape)
 print("pca explained variance ", pca.explained_variance_)
+plot_da(measured_pca, 'Principal Component Analysis')
 
-plt.figure()
-#plt.scatter(measured_pca[: , 0], measured_pca[: , 1], c=is_benign[unique_ids], cmap='seismic', alpha=0.8)
-plt.scatter(measured_pca[benign_ids , 0], measured_pca[benign_ids , 1], c='green', alpha=0.7, label='Benign')
-plt.scatter(measured_pca[malignant_ids , 0], measured_pca[malignant_ids , 1], c='red', alpha=0.7, label='Malignant')
-plt.xlabel('Principal Component 1')
-plt.ylabel('Principal Component 2')
-plt.title('Principal Component Analysis')
-plt.legend('Benign', 'Malignant')
-plt.legend()
+##################LDA#################
+lda = LDA(n_components=2)
+measured_lda = lda.fit(measured_spectra, labels).transform(measured_spectra)
+#plot_da(measured_lda, 'Linear Discriminant Analysis')
 
-
+##################QDA#################
+qda = QDA()
+measured_qda = qda.fit(measured_spectra, labels).predict(measured_spectra)
+#plot_da(measured_qda, 'Quadratic Discriminant Analysis')
 
 #head, *tail = Spectra
 
