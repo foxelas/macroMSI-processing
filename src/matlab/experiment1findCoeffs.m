34a21b5da89efad24357d76e10772d126a3ac757
foxelas <foxeln@outlook.com>
2020-05-18 17:33:54 +0900

    not sure

--- src/matlab/experiment1findCoeffs.m
+++ src/matlab/experiment1findCoeffs.m
@@ -1,28 +1,50 @@
 close all; clc; 
-k = 20;
+k =  17; %20;
 msiType = 'extended'; %'extended'; % 'max';
- 
+options.saveOptions.saveImages = false; 
+
 % % for all pixels without bg removal 
 % removebg = false; 
 % [msi, ~, ~, ~, ~, ~] = getImage(k, options, msiType, removebg, true);
 % [coeff, scores, latent,explained]  = doPixelPCA(msi);
 
-% % for only the pixels of the specimen
-% [msi, ~, specimenMask, height, width, ~] = getImage(k, options, msiType, false, true);
+[msiFull] = getImage(k, options, msiType, true);
+% plotMSI(msiFull, 1, options.saveOptions);
+% 
+% for only the pixels of the specimen
+[msi, ~, specimenMask, height, width, channels] = getImage(k, options, msiType, false, true);
+[msi, ~] = getDifMSI(msi, 'toAverage') ;
+
 specimenIdxs = find(specimenMask == 1)';
 colMsi = msi(specimenIdxs, :);
-[coeff,scores, latent,explained]  = doPixelPCA(colMsi);
-% [coeff,scores, latent,explained] = applyOnQualityPixels( @doPixelPCA, colMsi);
+%[coeff]  = doPixelPCA(colMsi);
+[coeff] = applyOnQualityPixels( @doPixelPCA, colMsi);
+
+centeredX = colMsi - mean(colMsi);
+sc = centeredX * coeff; 
 
 for i = 1 : 3
     pcComp = zeros(size(msi,1), 1);
-    pcComp(specimenIdxs) = scores(:,i);
+    pcComp(specimenIdxs) = sc(:,i);
     pcCompImage = reshape(pcComp, height, width);
     figure(i);
     imagesc(pcCompImage);
     colorbar;
     title(sprintf('Principal Component %d', i))
+    axis off;
+
     %caxis([-2,2])
+end 
+
+kCoeff = 2;
+for i = 1 : 9
+    w = zeros(channels,channels);
+    w(i,i) = kCoeff; 
+    enhancedImage = zeros(size(msi,1), channels);
+    healthySkinComponent = sum(sc(:,1:2),2) + mean(colMsi);
+    enhancedImage(specimenIdxs, :)  = (colMsi - healthySkinComponent) * w + colMsi ;
+    enhancedImage = reshape(enhancedImage', channels, height, width);
+    plotMSI(enhancedImage, 2, options.saveOptions);
 end 
 
 % pc1 healthy skin, pc2 hemoglobin, pc3 
